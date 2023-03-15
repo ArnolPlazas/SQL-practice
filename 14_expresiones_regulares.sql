fb4da5cc8d40977f49326cb71f3cc47450941aad
Arnol Plazas <plazasarnol@gmail.com>
2023-03-15 05:53:28 -0500

    expresiones regulares.

---
+++ 14_expresiones_regulares.sql
@@ -0,0 +1,8 @@
+-- Expresiones regulares
+SELECT email
+FROM platzi.alumnos
+WHERE email ~*'[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}';
+
+SELECT email
+FROM platzi.alumnos
+WHERE email ~*'[A-Z0-9._%+-]+@google[A-Z0-9.-]+\.[A-Z]{2,4}';
